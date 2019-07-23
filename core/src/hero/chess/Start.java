e960f8447eddfce0f2c41a72ae2342fb37441a0a
jhlopes <48145497+jhlopes@users.noreply.github.com>
2019-07-22 21:32:14 -0300

    Update Start.java

--- core/src/hero/chess/Start.java
+++ core/src/hero/chess/Start.java
@@ -2,8 +2,10 @@ package hero.chess;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -14,21 +16,23 @@ public class Start extends ApplicationAdapter {
 	SpriteBatch batch;
 	Sprite sprite;
 	Board board;
+	BitmapFont font;
 
 	@Override
 	public void create() {
 		batch = new SpriteBatch();
 
 		board = new Board(6, 8);
-
+		font = new BitmapFont();
 		Archer archer = new Archer("Archer", false, 1, 300, 30, 30, 200, 10);
 		board.insertCharacter(archer, 2, 2);
 		board.insertCharacter(archer, 2, 4);
 		board.insertCharacter(archer, 2, 4);
-		board.removeCharacetr(2,4);
-		board.removeCharacetr(2,4);
-		
-		
+		board.removeCharacetr(2, 4);
+		board.removeCharacetr(2, 4);
+		board.insertCharacter(archer, 2, 6);
+		board.insertCharacter(archer, 1, 3);
+
 	}
 
 	@Override
@@ -48,9 +52,14 @@ public class Start extends ApplicationAdapter {
 				float offset = (j % 2 == 0) ? 0 : (sprite.getWidth() / 2);
 				sprite = new Sprite(board.getSprite(i, j));
 				sprite.setSize(70, 70);
-				sprite.setPosition((i * sprite.getHeight()) + offset,
-						(j * ((sprite.getWidth() / 2) + sprite.getWidth() / 4)));
+				// float mod = ((sprite.getWidth() / 2) + sprite.getWidth() / 4);
+				float mod = (sprite.getHeight() - sprite.getHeight() / 4);
+				font.setColor(Color.BLACK);
+				
+				sprite.setPosition((i * sprite.getHeight()) + offset , (j * mod));
+				
 				sprite.draw(batch);
+				font.draw(batch, i + "    " + j, ((i * sprite.getHeight()) + (offset ) + 20 ), j * mod + 40 );
 			}
 		}
 	}
